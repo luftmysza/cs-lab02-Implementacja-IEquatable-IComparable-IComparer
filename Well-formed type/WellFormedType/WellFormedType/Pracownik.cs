3f93bffba02522497aabeed310ae50206600d288
luftmysza <111347760+luftmysza@users.noreply.github.com>
2024-04-14 02:24:05 +0200

    Step 1 & 2

---
+++ Well-formed type/WellFormedType/WellFormedType/Pracownik.cs
@@ -0,0 +1,158 @@
+﻿#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WellFormedType
+{
+    public class Pracownik : IEquatable<Pracownik>, IComparable<Pracownik>
+    {
+
+        private string _nazwisko;
+        private DateTime _dataZatrudnienia;
+        private decimal _wynagrodzenie;
+
+        public string Nazwisko
+        {
+            get => _nazwisko;
+            set
+            {
+                _nazwisko = value.Trim();
+            }
+        }
+
+        public DateTime DataZatrudnienia
+        {
+            get => _dataZatrudnienia;
+            set
+            {
+                if (value > DateTime.Today) throw new ArgumentException("100");
+                _dataZatrudnienia = value;
+            }
+        }
+
+        public decimal Wynagrodzenie
+        {
+            get => _wynagrodzenie;
+            set
+            {
+                if (value < 0) _wynagrodzenie = 0;
+                _wynagrodzenie = value;
+            }
+        }
+
+        public int CzasZatrudnienia
+        {
+            get
+            {
+                return (DateTime.Today.Year - DataZatrudnienia.Year) * 12 +
+                        (DateTime.Today.Month - DataZatrudnienia.Month) +
+                        (DateTime.Today.Day < DataZatrudnienia.Day ? -1 : 0);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"({Nazwisko}, {DataZatrudnienia:d MMM yyyy:}({CzasZatrudnienia}), {Wynagrodzenie} PLN)";
+        }
+
+        public override int GetHashCode() =>
+            (Nazwisko, DataZatrudnienia, Wynagrodzenie).GetHashCode();
+
+
+        public bool Equals(Pracownik other)
+        {
+            if (other == null) return false;
+            if (Object.ReferenceEquals(this, other)) return true;
+
+            return (Nazwisko == other.Nazwisko &&
+                    DataZatrudnienia == other.DataZatrudnienia &&
+                    Wynagrodzenie == other.Wynagrodzenie);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Pracownik pracownik)
+                return Equals(pracownik);
+            else
+                return false;
+        }
+
+        public static bool Equals(Pracownik some, Pracownik other)
+        {
+            if (some is null && other is null) return true;
+            if (some is null) return false;
+            
+            return some.Equals(other);
+        }
+
+        public static bool operator == (Pracownik some, Pracownik other) => 
+            Equals(some, other);
+
+        public static bool operator != (Pracownik some, Pracownik other) => 
+            !Equals(some, other);
+
+        public int CompareTo(Pracownik other)
+        {
+            if (other is null) return 1;
+            if (this.Equals(other)) return 0;
+
+            if (this.Nazwisko != other.Nazwisko)
+            {
+                if (StringComparer(this.Nazwisko, other.Nazwisko) == -1) return -1;
+                else if (StringComparer(this.Nazwisko, other.Nazwisko) == 1) return 1;
+            }
+            else if (this.DataZatrudnienia != other.DataZatrudnienia)
+            {
+                if (this.DataZatrudnienia < other.DataZatrudnienia) return -1;
+                else if (this.DataZatrudnienia > other.DataZatrudnienia) return 1;
+            }
+            else if (this.Wynagrodzenie != other.Wynagrodzenie)
+            {
+                if (this.Wynagrodzenie < other.Wynagrodzenie) return -1;
+                else if (this.Wynagrodzenie > other.Wynagrodzenie) return 1;
+            }
+            return 0;
+
+            int StringComparer(string given, string other)
+            {
+                
+                string shorter;
+
+                if (given.Length < other.Length) shorter = given;
+                else shorter = other;
+
+                for (int i = 0; i < shorter.Length; i++)
+                {
+                    if (given[i] < other[i]) return -1;
+                    if (given[i] > other[i]) return 1;
+                }
+                if (other.Length > given.Length) return 1;
+                return 0;
+            }
+        }
+
+        public Pracownik(string nazwisko,DateTime dataZatrudnienia, decimal wynagrodzenie)
+        {
+            Nazwisko = nazwisko;
+            DataZatrudnienia = dataZatrudnienia;
+            Wynagrodzenie = wynagrodzenie;
+        }
+
+        public Pracownik()
+        {
+            Nazwisko = "Anonim";
+            DataZatrudnienia = DateTime.Today;
+            Wynagrodzenie = 0;
+        }
+
+        
+
+
+    }
+}
---
+++ Well-formed type/WellFormedType/WellFormedType/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace WellFormedType
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("--- Sprawdzenie poprawności tworzenia obiektu ---");
+            Pracownik p = new Pracownik("Kowalski", new DateTime(2010, 10, 1), 1_000);
+            Console.WriteLine(p);
+
+            Console.WriteLine("--- Sprawdzenie równości obiektów ---");
+            Pracownik p1 = new Pracownik("Nowak", new DateTime(2010, 10, 1), 1_000);
+            Pracownik p2 = new Pracownik("Nowak", new DateTime(2010, 10, 1), 1_000);
+            Pracownik p3 = new Pracownik("Kowalski", new DateTime(2010, 10, 1), 1_000);
+            Pracownik p4 = p1;
+            Console.WriteLine($"p1: {p1} hashCode: {p1.GetHashCode()}");
+            Console.WriteLine($"p2: {p2} hashCode: {p2.GetHashCode()}");
+            Console.WriteLine($"p3: {p3} hashCode: {p3.GetHashCode()}");
+            Console.WriteLine($"p4: {p4} hashCode: {p4.GetHashCode()}");
+            Console.WriteLine();
+
+            Console.WriteLine($"--- Równość dla p1 oraz p2 -");
+            Console.WriteLine($"Object.ReferenceEquals(p1, p2): {Object.ReferenceEquals(p1, p2)}");
+            Console.WriteLine($"p1.Equals(p2): {p1.Equals(p2)}");
+            Console.WriteLine($"p1 == p2: {p1 == p2}");
+            Console.WriteLine();
+
+            Console.WriteLine($"--- Równość dla p1 oraz p3 -");
+            Console.WriteLine($"Object.ReferenceEquals(p1, p3): {Object.ReferenceEquals(p1, p3)}");
+            Console.WriteLine($"p1.Equals(p3): {p1.Equals(p3)}");
+            Console.WriteLine($"p1 == p3: {p1 == p3}");
+            Console.WriteLine();
+
+            Console.WriteLine($"--- Równość dla p1 oraz p4 -");
+            Console.WriteLine($"Object.ReferenceEquals(p1, p4): {Object.ReferenceEquals(p1, p4)}");
+            Console.WriteLine($"p1.Equals(p3): {p1.Equals(p4)}");
+            Console.WriteLine($"p1 == p4: {p1 == p4}");
+
+            List<Pracownik> Pracownicy = new List<Pracownik>
+            {
+                new Pracownik ("Sloniak", new DateTime(2010, 10, 1), 1000),
+                new Pracownik ("Jungiewicz", new DateTime(2010, 10, 1), 2500),
+                new Pracownik ("Paterna", new DateTime(2017, 10, 04), 10000),
+                new Pracownik ("Ramirez", new DateTime(2005, 11, 17), 500),
+                new Pracownik ("Niewiedzial", new DateTime(2011, 10, 11), 4000)
+            };
+
+            foreach(var pracownik in Pracownicy) Console.WriteLine(pracownik);
+
+            Pracownicy.Sort();
+            Console.WriteLine($"----");
+
+            foreach (var pracownik in Pracownicy) Console.WriteLine(pracownik);
+
+
+            Console.WriteLine('a' < 'b');
+
+            //List<string> Pracownicy1 = new List<string>
+            //{
+            //    "Sloniak",
+            //    "Jungiewicz",
+            //    "Paterna", 
+            //    "Ramirez",
+            //    "Niewiedzial"
+            //};
+
+            //foreach (var pracownik in Pracownicy1) Console.WriteLine(pracownik);
+
+            //Pracownicy1.Sort();
+            //Console.WriteLine($"----");
+
+            //foreach (var pracownik in Pracownicy1) Console.WriteLine(pracownik);
+        }
+    } 
+}
